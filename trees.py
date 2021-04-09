d728fd809abfb12f442d30d8fad5d424dee36792
StevenLovell <77277786+StevenLovell@users.noreply.github.com>
2021-04-09 14:03:04 +0100

    Delete trees.py

--- trees.py
+++
@@ -1,9 +0,0 @@
-from binarytree import tree,bst,heap
-# Generates a random tree, binary tree and a random heap.
-randomTree = tree(height=3, is_perfect=False)
-randomBst = bst(height=3, is_perfect=True)
-randomHeap = heap(height=3, is_max=True, is_perfect=False)
-
-print(randomTree)
-print(randomBst)
-print(randomHeap)
